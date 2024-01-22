38a14f16d0846b608edec807a11c1e82d4a694db
richardjim <richardiyevhobu@gmail.com>
2024-01-23 00:48:17 +0100

    init

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,16 +1,20 @@
-<form method="post" action="{{ route('register') }}" enctype="multipart/form-data">
-    @csrf
-    <label for="name">Name:</label>
-    <input type="text" name="name" required>
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" required>
+@extends('layouts.app')
 
-    <label for="password">Password:</label>
-    <input type="password" name="password" required>
+@section('content')
+    <form method="post" action="{{ route('register') }}" enctype="multipart/form-data">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required>
 
-    <label for="file">Profile Picture:</label>
-    <input type="file" name="file" accept="image/*" required>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
 
-    <button type="submit">Register</button>
-</form>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required>
+
+        <label for="file">Profile Picture:</label>
+        <input type="file" name="file" accept="image/*" required>
+
+        <button type="submit">Register</button>
+    </form>
+@endsection
